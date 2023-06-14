a927232458612db4446b50df9ec3936c45cacaaa
javier950107 <javier.rubio@uniat.edu.mx>
2023-06-14 17:56:31 -0600

    new

--- user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserController.java
+++ user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserController.java
@@ -1,14 +1,10 @@
 package com.uservideogames.uservideogames.controllers;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
