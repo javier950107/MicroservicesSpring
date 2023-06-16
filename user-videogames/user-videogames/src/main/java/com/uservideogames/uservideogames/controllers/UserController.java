4c2ed1762356a5200c1d28b63b43cfe6bb97387f
javier950107 <javier.rubio@uniat.edu.mx>
2023-06-16 17:56:09 -0600

    Unused library

--- user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserController.java
+++ user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.uservideogames.uservideogames.controllers;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +10,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
