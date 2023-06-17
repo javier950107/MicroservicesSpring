2bdf57bc8118d13ef1c4831c764b2d8c4c004290
Javier950107 <javier.rubio@uniat.edu.mx>
2023-06-17 00:20:39 -0600

    Refactor

--- user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/entity/User.java
+++ user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/entity/User.java
@@ -1,25 +1,18 @@
 package com.uservideogames.uservideogames.entity;
 
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import jakarta.persistence.Transient;
-import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
