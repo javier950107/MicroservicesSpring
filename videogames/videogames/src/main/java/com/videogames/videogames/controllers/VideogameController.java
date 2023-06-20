00e3e2bb20ec32b6f9d38fda0833d46c204808bc
javier950107 <javier.rubio@uniat.edu.mx>
2023-06-20 16:52:02 -0600

    Config server

--- videogames/videogames/src/main/java/com/videogames/videogames/controllers/VideogameController.java
+++ videogames/videogames/src/main/java/com/videogames/videogames/controllers/VideogameController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -20,6 +21,7 @@ import com.videogames.videogames.utils.ResponseFormat;
 
 import jakarta.validation.Valid;
 
+@RefreshScope
 @RestController
 public class VideogameController {
 
