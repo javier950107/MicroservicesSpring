3017e651997dc482b78bba8eff698509e1f80da8
javier950107 <javier.rubio@uniat.edu.mx>
2023-06-20 18:00:30 -0600

    Config server

--- user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserController.java
+++ user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -19,6 +20,7 @@ import com.uservideogames.uservideogames.utils.ResponseFormat;
 
 import jakarta.validation.Valid;
 
+@RefreshScope
 @RestController
 public class UserController {
     @Autowired
--- user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserVideogamesController.java
+++ user-videogames/user-videogames/src/main/java/com/uservideogames/uservideogames/controllers/UserVideogamesController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -23,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+@RefreshScope
 @RestController
 public class UserVideogamesController {
 
