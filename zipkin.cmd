27e6fe37e2d50d0a43da65e3f5316875613bdc53
Javier950107 <javier.rubio@uniat.edu.mx>
2023-06-27 23:55:52 -0600

    Zipkin config

--- zipkin.cmd
+++ zipkin.cmd
@@ -2,6 +2,6 @@
 set RABBIT_ADDRESSES=localhost:5672
 set STORAGE_TYPE=mysql
 set MYSQL_DB=zipkin
-set MYSQL_USER=zipkin
-set MYSQL_PASS=zipkin
+set MYSQL_USER=root
+set MYSQL_PASS=
 java -jar ./Zipkin/zipkin-server-2.24.2-exec.jar
